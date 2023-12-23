bed70a52c64b62f6dcd95da28017f7d968761d8a
HACHI3232 <harada.chisato526@gmail.com>
2023-12-23 15:13:20 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  get '/health' , to: 'health#show'
+  get '/health', to: 'health#show'
+  post '/todos', to: 'todos#create'
+  put '/todos/:id', to:'todos#update'
 end
